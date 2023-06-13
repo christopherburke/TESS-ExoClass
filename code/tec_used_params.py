227060bb1b46eb30309e281afbe7611a6e91d7dd
Christopher J. Burke <cjburke@mit.edu>
2023-06-13 09:08:10 -0400

    SPOC S64 as run

--- code/tec_used_params.py
+++ code/tec_used_params.py
@@ -1,15 +1,15 @@
 class tec_use_params:
     def __init__(self):
         self.singlesector = True
-        self.sector = 63
-        self.sector1 = 63
-        self.sector2 = 63
-        self.tecdir = 'sector63'
-        self.spocdir = 'sector-063'
-        self.tecfile = 'sector-63_20230421'
-        self.toifile = 'FIXED-20230421'
-        self.lcpre = 'tess2023069172124-s0063-'
-        self.lcnum = '0255'
-        self.dvpre = 'tess2023069204038-'
-        self.dvnum = '00712'
+        self.sector = 64
+        self.sector1 = 64
+        self.sector2 = 64
+        self.tecdir = 'sector64'
+        self.spocdir = 'sector-064'
+        self.tecfile = 'sector-64_20230612'
+        self.toifile = 'FIXED-20230612'
+        self.lcpre = 'tess2023096110322-s0064-'
+        self.lcnum = '0257'
+        self.dvpre = 'tess2023096143437-'
+        self.dvnum = '00720'
         self.resamp = 5
